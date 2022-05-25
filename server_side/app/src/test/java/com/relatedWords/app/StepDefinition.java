28636d909240a22e7d418d597856dedbae3fa6ec
Branchey <70140890+CZboop@users.noreply.github.com>
2022-05-25 16:26:11 +0100

    added cucumber integration tests for requests to get word and related words from database

--- server_side/app/src/test/java/com/relatedWords/app/StepDefinition.java
+++ server_side/app/src/test/java/com/relatedWords/app/StepDefinition.java
@@ -7,14 +7,17 @@ import io.cucumber.java.en.When;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StepDefinition {
     Integer wordId = null;
-    String name = null;
+    String word = null;
     private ResponseEntity response;
     RestTemplate restTemplate = new RestTemplate();
 
+//    find a word by id
     @Given("User gives id {int}")
     public void user_gives_word_id(Integer wordId) {
         this.wordId = wordId;
@@ -30,4 +33,51 @@ public class StepDefinition {
         assertEquals(statusCode, response.getStatusCodeValue());
     }
 
+//   retrieve related words given word
+    @Given("User gives word {string}")
+    public void user_gives_word_as_string(String word) {
+        this.word = word;
+    }
+
+    @When("The user makes a call to {string} to get related words")
+    public void the_user_makes_a_call_to_get_related_words(String url) {
+        response = restTemplate.getForEntity(url + word, List.class);
+    }
+
+    @Then("The API should return a list of related words and response code {int}")
+    public void the_api_should_return_the_list_of_words_and_response_code(int statusCode) {
+        assertEquals(statusCode, response.getStatusCodeValue());
+    }
+
+//    getting a single related word given a word
+    @Given("User gives a word {string}")
+    public void user_gives_a_word_as_string(String word) {
+        this.word = word;
+    }
+
+    @When("The user makes a call to {string} to get a related word")
+    public void the_user_makes_a_call_to_get_a_related_word(String url) {
+        response = restTemplate.getForEntity(url + word + "/random", Object.class);
+    }
+
+    @Then("The API should return a related word and response code {int}")
+    public void the_api_should_return_a_word_and_response_code(int statusCode) {
+        assertEquals(statusCode, response.getStatusCodeValue());
+    }
+
+//    getting a word with same part of speech
+    @Given("User gives a word {string} to find a related word with the same POS")
+    public void user_gives_a_word_as_string_to_find_same_pos(String word) {
+        this.word = word;
+    }
+
+    @When("The user makes a call to {string} to get a related word with the same POS")
+    public void the_user_makes_a_call_to_get_a_related_word_with_same_pos(String url) {
+        response = restTemplate.getForEntity(url + word + "/pos", Object.class);
+    }
+
+    @Then("The API should return a related word with the same POS and response code {int}")
+    public void the_api_should_return_a_word_with_same_pos_and_response_code(int statusCode) {
+        assertEquals(statusCode, response.getStatusCodeValue());
+    }
 }
--- server_side/app/src/test/resources/word.feature
+++ server_side/app/src/test/resources/word.feature
@@ -3,3 +3,18 @@ Feature: Word Get Information
     Given User gives id 1
     When The user makes a call to "http://localhost:8080/api/word/get/" get the details
     Then The API should return the word and response code 200
+
+  Scenario: When word given related words are retrieved from the database
+    Given User gives word "example"
+    When The user makes a call to "http://localhost:8080/api/word/related/" to get related words
+    Then The API should return a list of related words and response code 200
+
+  Scenario: When word given a single related word can be retrieved from the database
+    Given User gives a word "word"
+    When The user makes a call to "http://localhost:8080/api/word/related/" to get a related word
+    Then The API should return a related word and response code 200
+
+  Scenario: When word given related word with the same POS can be retrieved from the database
+    Given User gives a word "word" to find a related word with the same POS
+    When The user makes a call to "http://localhost:8080/api/word/related/" to get a related word with the same POS
+    Then The API should return a related word with the same POS and response code 200
